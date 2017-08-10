d25e71d0b2efccbe1517f3665da7690be7a3f593
shloid (da man!!!) <shloiddaman@outlook.com>
2017-08-10 07:48:16 -0700

    Updated BulletScript for future use.

--- GameplayScripts/GunSystem/BulletScript.lua
+++ GameplayScripts/GunSystem/BulletScript.lua
@@ -1,65 +1,61 @@
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- BulletScript
+-- Created by clockwork, rescripted by shloid (da man!!!)
+-- also i really hate this script. but thank you dogu for the help <3
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Services
+local Debris = game:GetService("Debris")
+local Players = game:GetService("Players")
+local RepStorage = game:GetService("ReplicatedStorage")
+
+-- Self
 local ball = script.Parent
-local r_storage = game:GetService("ReplicatedStorage")
-local _Gv2 = require(r_storage:FindFirstChild("_Gv2"))
-damageTag = ball.Damage
+local _Gv2 = require(RepStorage:FindFirstChild("_Gv2"))
+local CreatorV = ball:WaitForChild("creator")
+local Creator = CreatorV.Value
+local damageTag = 45
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Functions / Methods
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 function makeFlak()
-	for i=1,1 do
-		local s = Instance.new("Part")
-		s.Shape = 1 -- block
-		s.formFactor = 2 -- plate
+	for i = 1, 1 do
+		local s = Instance.new("Part",workspace)
 		s.Size = Vector3.new(1,.4,1)
-		s.BrickColor = BrickColor.Black()
+		if ball.Name == "SoulBolt" then
+			s.BrickColor = Color3.new(255/255,255/255,255/255)
+		else
+			s.BrickColor = Color3.new(0,0,0)
+		end
 		local v = Vector3.new(math.random(-1,1), math.random(0,1), math.random(-1,1))
 		s.Velocity = 15 * v
 		s.CFrame = CFrame.new(ball.Position - ball.Velocity.unit * 10, ball.Position + v)
-		s.Parent = game.Workspace
-		game:service("Debris"):AddItem(s, .3)
+		Debris:AddItem(s, .3)
 	end
 end
 
-function onTouched(hit)
-	if hit.Parent == ball.creator.Value.Character then return end
-	if hit.Parent.Parent == ball.creator.Value.Character then return end
-	if hit.Parent.Parent.Parent == ball.creator.Value.Character then return end
-	if hit.Parent == nil then return end
-	if hit.Name == "Bullet" then return end
-	if hit.Name == "Handle" then return end
-
-	local humanoid = hit.Parent:findFirstChild("Humanoid")
-
-	if humanoid == nil then
-		humanoid = hit.Parent.Parent:findFirstChild("Humanoid")
-	end
-
-	if humanoid ~= nil then
-		_Gv2:tagHumanoid(ball, humanoid)
-
-		local tag = ball:findFirstChild("creator")
-		if hit.Name == "Head" then
-			damageTag.Value = damageTag.Value * 2
-		end
-
-		-- VAMPIRIC VIM
-		if humanoid.Parent.Class.Value == "Vampire" then
-			if hit.Name ~= "Head" and hit.Name ~= "Torso" then
-				damageTag.Value = damageTag.Value / 2
+ball.Touched:connect(function(hit)
+	if hit.Parent == nil or hit.Parent == Creator.Character then makeFlak() return end
+	local hum = hit.Parent:FindFirstChild("Humanoid")
+	if hum ~= nil then
+		if hit.Parent:FindFirstChild("Class") then
+			local hitClass = hit.Parent:FindFirstChild("Class")
+			if _Gv2.DemonPeople[hitClass.Value] and ball.Name == "SoulBolt" then
+				damageTag = 67.5
 			end
 		end
-
-		_Gv2:DealDamage(tag.Value.Character, humanoid.Parent, damageTag.Value)
+		_Gv2:DealDamage(Creator.Character, hit.Parent, damageTag)
+		makeFlak()
 	else
 		makeFlak()
 	end
+	ball:Destroy()
+end)
 
-	ball:remove()
-end
-
-connection = ball.Touched:connect(onTouched)
-
-delay(5, function() ball.Parent = nil end)
-
-while true do
-	wait(.25)
-	ball.CFrame = CFrame.new(ball.Position, ball.Position + ball.Velocity.unit)
-end
+spawn(function()
+	while wait(.3) do
+		ball.CFrame = CFrame.new(ball.Position, ball.Position + ball.Velocity.unit)
+	end
+end)
