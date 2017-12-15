868d3d8fa6f42e71ab9c901f486230018171c805
howmanysmall <9477871+1@iechs.org>
2017-12-15 13:00:52 -0700

    Update PersonScript.lua

--- GameplayScripts/PersonScript.lua
+++ GameplayScripts/PersonScript.lua
@@ -10,7 +10,7 @@ local Player = (Players:GetPlayerFromCharacter(Character) or nil)
 
 -------------------------------------------------------------------------------------------------------------------------------------------
 if Player == nil then 
-	print("[Server] PersonScript has detected that the object, known as",Character.Name,"is not a valid Character")
+	print("[Server] PersonScript has detected that the object, known as ", Character.Name, " is not a valid Character")
 	print("[Server] Disabling this script instance...")
 	script.Disabled = true
 end
@@ -26,16 +26,16 @@ local Backpack = Player:WaitForChild("Backpack")
 
 function updateBuffs()
 	local buffs = Character.Buffs:GetChildren()
-	local names = {}
-	for i,v in ipairs(buffs) do
-		table.insert(names, v.Name)
+	local names = { }
+	for i, v in pairs(buffs) do --ipairs
+		names[#names + 1] = v.Name
 	end
-	if (#names > 0) then
+	if #names > 0 then
 		Status.Text = table.concat(names, ", ")
 	else
 		Status.Text = "Nothing."
 	end
 end
 
-Character.Buffs.ChildAdded:connect(updateBuffs)
-Character.Buffs.ChildRemoved:connect(updateBuffs)
+Character.Buffs.ChildAdded:Connect(updateBuffs)
+Character.Buffs.ChildRemoved:Connect(updateBuffs)
