280a001567e2a15d2aa32ebfa902ae34cd01fa48
howmanysmall <9477871+1@iechs.org>
2017-12-15 13:11:35 -0700

    Update GrenadeScript.lua

--- GameplayScripts/GunSystem/GrenadeScript.lua
+++ GameplayScripts/GunSystem/GrenadeScript.lua
@@ -1,24 +1,22 @@
--------------------------------------------------------------------------------------------------------------------------------------------
--- Kiseki CTF: Classic Edition Gun System
--- Scripted by Clockwork, Modified by shloid
--------------------------------------------------------------------------------------------------------------------------------------------
+local game = game
+local require = require
+local script = script
+local wait = wait
+local spawn = spawn
+local Instance = Instance local Instance_new = Instance.new
+--@services
+local Workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+--@main
+local _Gv2 = require(ReplicatedStorage:FindFirstChild("_Gv2"))
 
--- Services
-local Rep = game:GetService("ReplicatedStorage")
-local _Gv2 = require(Rep:FindFirstChild("_Gv2"))
-
--- Variables
 local Ball = script.Parent
 local damageTag = Ball.Damage
-local CreatorV = Ball:findFirstChild("creator")
+local CreatorV = Ball:FindFirstChild("creator")
 local Creator = CreatorV.Value
-local ExplosionSound = (Ball:FindFirstChild("Explosion") or nil)
+local ExplosionSound = Ball:FindFirstChild("Explosion") or nil
 
--------------------------------------------------------------------------------------------------------------------------------------------
--- Functions / Methods
--------------------------------------------------------------------------------------------------------------------------------------------
-
-function onTouched(hit)
+local function OnTouched(Hit)
 	if hit.Parent == nil then return end
 	local humanoid = hit.Parent:findFirstChild("Humanoid")
 	if humanoid then
@@ -31,26 +29,25 @@ function onTouched(hit)
 	end
 end
 
-function explode(hit)
-	connection:disconnect()
-	if ExplosionSound ~= nil then
+local function explode(hit)
+	connection:Disconnect()
+	if ExplosionSound then
 		ExplosionSound:Play()
 	end
-	
-	local explosion = Instance.new("Explosion",workspace)
+	local explosion = Instance_new("Explosion")
 	explosion.BlastPressure = 10000
 	explosion.DestroyJointRadiusPercent = 0
 	explosion.BlastRadius = 12
 	explosion.Position = Ball.Position
-	explosion.Hit:connect(function(part, distance)
-		 onTouched(part)
+	explosion.Parent = Workspace
+	explosion.Hit:Connect(function(part, distance)
+		 OnTouched(part)
 	end)
-	
-	wait(.3)
+	wait(0.3)
 	Ball:Destroy()
 end
 
-connection = Ball.Touched:connect(explode)
+connection = Ball.Touched:Connect(explode)
 
 spawn(function()
 	wait(5)
